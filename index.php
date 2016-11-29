4d80aae348ee92954bd99817a7fe057c7d435e26
BrylaQ <bartylak.michal@gmail.com>
2016-11-29 17:44:10 +0100

    Revert "Revert "Orient""
    This reverts commit 9aa12c0ceca0ce304f1570774187ce8544ed4394.

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+	session_start();
+	if(isset($_SESSION['logedIn'])) 
+	{
+		header("Location:explorer.php");
+		exit();
+	}
+?>
+
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>PAS Z7</title>
+	<link rel="stylesheet" href="main.css">
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu+Mono:400,700&amp;subset=latin-ext" rel="stylesheet">
+</head>
+
+<body>
+	<div id="container">
+		<div class="title">Log in</div>
+		<div class="underline"></div>
+		
+		<form method="POST" action="login.php">
+		<br>
+		Login:<br><input type="text" name="user"
+		<?php if(isset($_COOKIE['user'])) echo "value=".$_COOKIE['user']; ?>><br>
+		Password:<br><input type="password" name="password"><br>
+		<input type="submit" value="Log in"/>
+		</form>
+		
+		<?php
+			if(isset($_SESSION['err']))
+			{
+				echo $_SESSION['err'];
+				unset($_SESSION['err']);
+			}
+		?>
+		
+		<!--<a href="register.php">
+		<div class="button">Register</div>
+		</a>-->
+	
+	</div>
+</body>
+</html>
