7c41c08935c371c1450175039c8499a97f81e285
BrylaQ <bartylak.michal@gmail.com>
2016-11-30 21:48:02 +0100

    improoved file listing
    fontello
    buttons hrefs

--- explorer.php
+++ explorer.php
@@ -14,6 +14,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<title>PAS Z7</title>
 	<link rel="stylesheet" href="main.css">
+	<link rel="stylesheet" href="css/fontello.css">
 	<link href="https://fonts.googleapis.com/css?family=Ubuntu+Mono:400,700&amp;subset=latin-ext" rel="stylesheet">
 </head>
 
@@ -29,31 +30,42 @@
 				echo $_SESSION['warn'];
 				unset($_SESSION['warn']);
 			}
-			
-			$path = './users/'.$_COOKIE['user']."/";
-			
-			echo "<div style='width:300px;text-align:center;margin-top: 100px;margin-left:auto;margin-right:auto;'><table>";
-			foreach (new DirectoryIterator($path) as $file) 
+		?>
+		
+		<form action="upload.php" method="post" enctype="multipart/form-data">
+			Select file to upload:
+			<input type="file" name="fileToUpload" id="fileToUpload">
+			<input type="submit" value="Upload" name="submit">
+		</form>
+		
+		<?php
+		$path = './users/'.$_COOKIE['user']."/";
+		
+		echo "<div style='width:300px;text-align:center;margin-top: 50px;margin-left:auto;margin-right:auto;'><table cellspacing='0'>";
+		foreach (new DirectoryIterator($path) as $file) 
+		{
+			if ($file->isDir())
 			{
-				if ($file->isDir())
-				{
-					if($file->getFilename() !="." && $file->getFilename() !="..")
-					{
-						echo "<tr><td>";
-						print $file->getFilename() . "<br>";
-						echo "</td><td><div class='buttonsm'>EDIT<div></td></tr>";
-					}
-				}
-				elseif ($file->isFile()) 
+				if($file->getFilename() !="." && $file->getFilename() !="..")
 				{
 					echo "<tr><td>";
 					print $file->getFilename() . "<br>";
 					echo "</td><td><div class='buttonsm'>EDIT<div></td></tr>";
 				}
 			}
-			echo "</table></div>";
+			elseif ($file->isFile()) 
+			{
+				echo "<tr><td>";
+				$filename = $file->getFilename();
+				echo $filename."</td>";
+				echo "
+				<td><a href='delete.php?filename=$filename'><div class='buttonsm' id='$filename'><i class='icon-trash-empty'></i><div></a></td>
+				<td><a href='download.php?filename=$filename'><div class='buttonsm' id='$filename'><i class='icon-download-cloud'></i><div></a></td></tr>";
+			}
+		}
+		echo "</table></div>";
 			
-		?>
+	?>
 
 	</div>
 </body>
---
+++ upload.php
@@ -0,0 +1,44 @@
+<?php
+	session_start();
+	if(!isset($_COOKIE['logedIn'])) 
+	{
+		header("Location:index.php");
+		exit();
+	}
+	
+	$user = $_COOKIE['user'];
+	$target_dir = "./users/".$user."/";
+	$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+	$uploadOk = 1;
+	$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
+	if (file_exists($target_file)) 
+	{
+		$_SESSION["warn"] = "<div class='err'>Sorry, file already exists.</div>";
+		$uploadOk = 0;
+	}
+	
+	if ($_FILES["fileToUpload"]["size"] > 100000) 
+	{
+		$_SESSION["warn"] = "<div class='err'>Sorry, your file is too large.</div>";
+		$uploadOk = 0;
+	}
+	
+
+	if ($uploadOk == 0) 
+		header("Location:explorer.php");
+
+	else 
+	{
+		if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) 
+		{
+			$_SESSION["warn"] = "<div class='err' style='color:#093;'>The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.</div>";
+		} 
+		else 
+		{
+			$_SESSION["warn"] = "<div class='err'>Sorry, there was an error uploading your file.</div>";
+			header("Location:explorer.php");
+		}
+		header("Location:explorer.php");
+	}
+?>
