154a0993f8a3fecfd5b22cab366ae913e10ba5db
BrylaQ <bartylak.michal@gmail.com>
2016-12-01 01:17:03 +0100

    creating directory

---
+++ createDirectory.php
@@ -0,0 +1,22 @@
+<?php
+	session_start();
+	if(!isset($_COOKIE['logedIn'])) 
+	{
+		header("Location:index.php");
+		exit();
+	}
+	
+	$user = $_COOKIE['user'];
+	$target_dir = "./users/".$user."/";
+	if(isset($_GET['filename']))
+		$target_dir = $_GET['filename'];
+	$target_dir = $target_dir.$_POST['dirName'];
+	header("Location:explorer.php");
+	if(!is_dir($target_dir))
+	{
+		mkdir($target_dir);
+		$_SESSION["warn"] = "<div class='err' style='color:#093;'>Directory created successfully!</div>";
+	}
+	else
+		$_SESSION["warn"] = "<div class='err'>Directory cannot be created!</div>";
+?>
